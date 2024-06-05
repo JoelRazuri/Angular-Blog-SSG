b830dda0dbfc8fe59a220a634e7922a6263fe1c6
Joel Razuri <jrazuri98@hotmail.com>
2024-06-04 23:40:13 -0300

    HeaderService en home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostPreviewComponent } from './components/post-preview/post-preview.component';
+import { HeaderData, HeaderService } from '../../services/header.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,16 @@ import { PostPreviewComponent } from './components/post-preview/post-preview.com
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  private uiData: HeaderData = {
+    title: 'Nuestro excelente blog',
+    subtitle: 'Este es el subtitulo',
+    thumbnail: 'https://placehold.art/1200x400'
+  }
+  constructor(private headerService: HeaderService) { }
+  
+  ngOnInit(){
+    this.headerService.uiData.set(this.uiData)
+  }
 
 }
